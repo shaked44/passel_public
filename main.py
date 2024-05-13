80f940a0f701160ca4ee528686c77f9feec1771b
shaked44 <shakedever@gmail.com>
2024-05-13 15:17:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -212,4 +212,4 @@ async def on_guild_channel_pins_update(channel, last_pin):
 #client.run('MTAwOTEyNjIzMTU4Njg5ODAyMg.GOPYKu.B3uxCiiU9IEOp3mWgoPe5Rq6b6w5Ynptc25Ews')
 
 # TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
-client.run(os.environ.get('TOKEN'))
+client.run(os.environ.get('MTIzOTUzNTMwOTAzODAzMDkxOA.G7Ipr9.XQfgjPvtDOIAe9uho58ajrH2APih3bbxT5KO-I'))
